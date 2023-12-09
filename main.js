a9d388ac7603e4ee7e75785cfe0d79a29d67c056
Kunal-2010 <108459968+Kunal-2010@users.noreply.github.com>
2023-12-09 14:46:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-function preload() {
-
+function preload(){
 }
 
-function setup() {
-    canvas = createCanvas(300, 300);
+function setup(){
+    canvas = createCanvas(300,300);
     canvas.center();
-}
-function draw() {
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function take_snapshot() {
-    save("myphtho.png");
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
